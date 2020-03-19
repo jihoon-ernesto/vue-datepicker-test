18022518a36d653c35cd811c0521f05bbda8a22f
jihoon <jihoon@hancom.com>
2020-03-19 17:21:22 +0900

    Intro vue-functional-calendar

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <VCalDatePicker></VCalDatePicker>
     <CtkDateTimePicker></CtkDateTimePicker>
+    <FunctionalCal></FunctionalCal>
   </div>
 </template>
 
 <script>
 import VCalDatePicker from './components/VCalDatePicker.vue';
 import CtkDateTimePicker from './components/CtkDateTimePicker.vue';
+import FunctionalCal from './components/FunctionalCal.vue';
 
 export default {
   name: 'App',
   components: {
     VCalDatePicker,
     CtkDateTimePicker,
+    FunctionalCal,
   }
 }
 </script>
---
+++ src/components/FunctionalCal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>FunctionalCalendar test</p>
+    <FunctionalCalendar
+      v-model='calendarData'
+      :configs='calendarConfigs'
+    ></FunctionalCalendar>
+  </div>
+</template>
+
+<script>
+import { FunctionalCalendar } from 'vue-functional-calendar';
+
+export default {
+  name: 'FunctionalCal',
+  props: {
+  },
+  components: {
+    FunctionalCalendar,
+  },
+  data() {
+    return {
+      calendarData: {},
+      calendarConfigs: {
+        isModal: true,
+        isDateRange: true,
+        changeYearFunction: true,
+        changeMonthFunction: true,
+      },
+    };
+  }
+}
+</script>
+
+<style scoped>
+</style>
