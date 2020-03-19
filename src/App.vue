de7f265ffb12e9bba744e67e60ec26c70d1a5ebf
jihoon <jihoon@hancom.com>
2020-03-19 14:26:59 +0900

    intro V-Calendar date picker
    - and remove HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VCalDatePicker></VCalDatePicker>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VCalDatePicker from './components/VCalDatePicker.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VCalDatePicker,
   }
 }
 </script>
---
+++ src/components/VCalDatePicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>V-Calendar Date Picker test</p>
+    <v-date-picker
+      v-model='dateRange'
+      mode='range'
+      :masks='masks'
+      />
+  </div>
+</template>
+
+<script>
+import DatePicker from 'v-calendar/lib/components/date-picker.umd'
+
+export default {
+  name: 'VCalDatePicker',
+  props: {
+    initDate: String
+  },
+  components: {
+    'v-date-picker': DatePicker,
+  },
+  data() {
+    return {
+      dateRange: {
+        start: new Date(2020, 2, 19),
+        end: new Date(2020, 3, 5),
+      },
+      masks: {
+        input: ['YYYY-MM-DD'],
+      },
+    };
+  }
+}
+</script>
+
+<style scoped>
+</style>
